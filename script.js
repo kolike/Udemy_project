7e5efbd832880860e5032fa0f2a22499ce24cfa2
Nikolay <kolik_2604@mail.ru>
2022-06-28 10:54:29 +0300

    Добавлены новые функции

--- script.js
+++ script.js
@@ -1,17 +1,71 @@
-var numberOfFilms = +prompt('Сколько фильмов вы ужек посмотрели?', '');
-var personalMoveDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-var
-    a = prompt('Один из последних фильмов?'),
-    b = prompt('Ваша оценка?'),
-    c = prompt('Один из последних фильмов?'),
-    d = prompt('Ваша оценка?');
+(function () {
+    "use strict";
+    let numberOfFilms;
 
-personalMoveDB.movies[a] = b;
-personalMoveDB.movies[c] = d;
-console.log(personalMoveDB);
+
+    start();
+    writeYourGenres();
+    rememberMyFilms();
+    detectPersonalLevel();
+
+    function start() {
+        numberOfFilms = +prompt('Skoka filmov posmotrel?');
+
+        while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+            numberOfFilms = +prompt('Skoka filmov posmotrel?');
+        }
+    }
+    const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
+    showMyDB(personalMovieDB.privat);
+
+    function showMyDB(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    }
+
+
+    function writeYourGenres() {
+        for (let i = 0; i <= 2; i++) {
+            personalMovieDB.genres[i] = prompt(`Nazovite vash lyubimiy ganr # ${i+1}`);
+        }
+    }
+
+
+
+    function rememberMyFilms() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Posledniy prosmotrenniy film?'),
+                b = prompt('Ocenka filma?');
+
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    }
+
+
+
+    function detectPersonalLevel() {
+        if (personalMovieDB.count < 10) {
+            console.log('malo filmov smotrel');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log('Klassika, ' + personalMovieDB.count + ' filmov');
+        } else if (personalMovieDB.count >= 30) {
+            console.log('pogulyat shodi, zadrot');
+        } else {
+            console.log('ERORR ERROR ERROR');
+        }
+    }
+
+}());
